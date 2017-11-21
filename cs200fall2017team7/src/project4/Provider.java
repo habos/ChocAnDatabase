6af8d3123e25bf9f8e1fd83bc4807a91ccf27668
ColinMacwilliam <comacwilliam@crimson.ua.edu>
2017-11-21 12:10:56 -0600

    commentz

--- cs200fall2017team7/src/project4/Provider.java
+++ cs200fall2017team7/src/project4/Provider.java
@@ -1,10 +1,17 @@
 package project4;
 
+/**
+ * The provider class. This is a entity stereotype class.
+ * @author Colin 
+ */
+
 public class Provider extends Record {
+	
 	protected int numberID;
 	protected String name;
 	protected String address;
 	protected String city;
 	protected String state;
 	protected String ZIP;
+	
 }
