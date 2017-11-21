7b38c97d7d02917b798420f17cb2c41d9aa36a24
habos <habos@crimson.ua.edu>
2017-11-21 12:02:07 -0600

    Base member report done

--- cs200fall2017team7/src/project4/MemberReport.java
+++ cs200fall2017team7/src/project4/MemberReport.java
@@ -4,14 +4,22 @@ public class MemberReport {
 	
 	private Member member;
 	
+	/**
+	 * Constructor
+	 * @param mem = a member object
+	 */
 	public MemberReport(Member mem){
 		member = mem;
 	}
 	
+	/**
+	 * toString function that will pass the report as a string
+	 */
 	public String toString(){
 		String memberReportString = "";
-		
-		return "";
+		memberReportString = member.toString();
+		//Once ClaimsDatabase is done add all the member claims here
+		return memberReportString;
 	}
 	
 }
