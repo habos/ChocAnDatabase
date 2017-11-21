bcc5bb56a0a930b98df7ded6b07e7514d7849cfd
habos <habos@crimson.ua.edu>
2017-11-21 11:55:10 -0600

    fixed member and added member report

---
+++ cs200fall2017team7/src/project4/MemberReport.java
@@ -0,0 +1,17 @@
+package project4;
+
+public class MemberReport {
+	
+	private Member member;
+	
+	public MemberReport(Member mem){
+		member = mem;
+	}
+	
+	public String toString(){
+		String memberReportString = "";
+		
+		return "";
+	}
+	
+}
