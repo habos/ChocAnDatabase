e8a25adec6010bfa9043b738f57c0542963da19c
Lgmimms <lgmimms@crimson.ua.edu>
2017-11-28 11:08:11 -0600

    Work on summary reporr

---
+++ cs200fall2017team7/src/project4/SummaryReport.java
@@ -0,0 +1,48 @@
+package project4;
+
+public class SummaryReport extends Report{
+
+	protected int providerTotalFee;
+	protected int overallTotal;
+	protected int providerConsultations;
+	
+	protected int numProvidersServices;// number of providers who gave service
+	protected int totalConsultations;
+	protected int totalFees;
+	
+	protected String providerName;
+	
+	public SummaryReport(ProvidersDatabase providers) {
+		 
+		report = "";
+		
+		providerTotalFee = 0;
+		overallTotal = 0;
+		providerConsultations = 0;
+		
+		totalConsultations = providers.getTotalConsultant();
+		totalFees = providers.getTotalFee();
+		numProvidersServices = 0;
+		
+		
+		for(int i =0; i<providers.getSize(); i++) 
+		{
+			if(providers.getIndivFee(i) != 0)
+			{
+				
+				numProvidersServices += providers.getConsultant(i);
+				
+				providerTotalFee = providers.getIndivFee(i);
+				providerConsultations = providers.getConsultant(i);
+				providerName = providers.getName(i);
+				
+				
+			}
+		}
+		
+		
+		
+	}
+	
+	
+}
