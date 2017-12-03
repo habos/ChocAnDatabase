18ebc614fd578a36c5592428c30b0b6d6c8491ab
calebwerth <cwerth@crimson.ua.edu>
2017-12-03 15:50:13 -0600

    Changed "payed" to "paid"

--- cs200fall2017team7/src/project4/EFT.java
+++ cs200fall2017team7/src/project4/EFT.java
@@ -4,7 +4,7 @@ public class EFT extends Report{
 
 	public EFT(String providerName, int providerID, int price ){
 		
-		report = "Provider Name: "+providerName+"\nProvider ID: "+providerID+"\nFee to be payed: "+price+"\n";
+		report = "Provider Name: "+providerName+"\nProvider ID: "+providerID+"\nFee to be paid: "+price+"\n";
 		
 	}
 	
