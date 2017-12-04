b0b36c26fb2ac1984d305991476c338e3833dee4
habos <habos@crimson.ua.edu>
2017-12-03 18:12:51 -0600

    fixed an error that someone created

--- cs200fall2017team7/src/project4/MembersDatabase.java
+++ cs200fall2017team7/src/project4/MembersDatabase.java
@@ -36,7 +36,6 @@ public class MembersDatabase extends Database {
 		// Create and add the Member to MembersDatabase
 		records.add(new Member(id, name, address, city, state, ZIP));
 		// Close the scanner
-		//scan.close();
 	}
 
 	/**
