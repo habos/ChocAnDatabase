87eb3c258fcada022a350153e53f225c35fd7fdf
ColinMacwilliam <comacwilliam@crimson.ua.edu>
2017-12-03 18:08:03 -0600

    removed all comments from getters and setters

--- cs200fall2017team7/src/project4/Record.java
+++ cs200fall2017team7/src/project4/Record.java
@@ -29,82 +29,58 @@ public abstract class Record {
 				claims= new ArrayList<Claim>();
 			}
 	
-	/**
-	 * @return the numberID
-	 */
+
 	public int getId() {
 		return id;
 	}
-	/**
-	 * @param numberID the numberID to set
-	 */
+	
 	public void setNumberID(int id) {
 		this.id = id;
 	}
-	/**
-	 * @return the name
-	 */
+
 	public String getName() {
 		return name;
 	}
-	/**
-	 * @param name the name to set
-	 */
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	/**
-	 * @return the address
-	 */
+
 	public String getAddress() {
 		return address;
 	}
-	/**
-	 * @param address the address to set
-	 */
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	/**
-	 * @return the city
-	 */
+
 	public String getCity() {
 		return city;
 	}
-	/**
-	 * @param city the city to set
-	 */
+
 	public void setCity(String city) {
 		this.city = city;
 	}
-	/**
-	 * @return the state
-	 */
+
 	public String getState() {
 		return state;
 	}
-	/**
-	 * @param state the state to set
-	 */
+
 	public void setState(String state) {
 		this.state = state;
 	}
-	/**
-	 * @return the zIP
-	 */
+
 	public String getZIP() {
 		return ZIP;
 	}
-	/**
-	 * @param zIP the zIP to set
-	 */
+
 	public void setZIP(String zIP) {
 		ZIP = zIP;
 	}
 	/**
 	 * 
 	 * @param id to be compared
-	 * @return if matching id's
+	 * @return boolean if matching id's
 	 */
 	public boolean matches(int id) {
 		if(id==this.id)
